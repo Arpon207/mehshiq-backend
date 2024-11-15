d8cf72215eb5569340143ff6136acd0789939e00
Arpon207 <aarpon207@gmail.com>
2024-11-15 19:09:40 +0600

    product filtering

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -9,6 +9,42 @@ export const getAllProducts = async (req, res) => {
   }
 };
 
+export const newProducts = async (req, res) => {
+  try {
+    const result = await productsModel.find().sort({ createdAt: -1 }).limit(10);
+    res.send(result);
+  } catch (error) {
+    console.log(e.message);
+  }
+};
+
+export const bestSellerProducts = async (req, res) => {
+  try {
+    const result = await productsModel.find().sort({ sold: -1 }).limit(14);
+    res.send(result);
+  } catch (error) {
+    console.log(e.message);
+  }
+};
+
+export const getWomensProducts = async (req, res) => {
+  try {
+    const result = await productsModel.find().limit(10);
+    res.send(result);
+  } catch (error) {
+    console.log(e.message);
+  }
+};
+
+export const getMensProducts = async (req, res) => {
+  try {
+    const result = await productsModel.find().limit(10);
+    res.send(result);
+  } catch (error) {
+    console.log(e.message);
+  }
+};
+
 export const getProductById = async (req, res) => {
   try {
     const result = await productsModel.findById(req.params.id);
@@ -35,9 +71,9 @@ export const getProductsByCategory = async (req, res) => {
   try {
     const result = await productsModel
       .find(filter)
-      .sort(sort)
       .skip((currentPage - 1) * limitPerPage)
-      .limit(limitPerPage);
+      .limit(limitPerPage)
+      .sort(sort);
     const count = await productsModel.countDocuments(filter);
     res.send({ result, count });
   } catch (error) {
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,13 @@
 import express from "express";
 import {
+  bestSellerProducts,
   getAllProducts,
+  getMensProducts,
   getProductById,
   getProductsByCategory,
   getProductsByQuery,
+  getWomensProducts,
+  newProducts,
 } from "../controllers/products.controller.js";
 
 const router = express.Router();
@@ -15,5 +19,13 @@ router.get("/product/:id", getProductById);
 router.get("/productsByCategory", getProductsByCategory);
 
 router.get("/searchByQuery", getProductsByQuery);
+
+router.get("/newArrivals", newProducts);
+
+router.get("/bestSellerProducts", bestSellerProducts);
+
+router.get("/womens", getWomensProducts);
+
+router.get("/mens", getMensProducts);
 
 export default router;
