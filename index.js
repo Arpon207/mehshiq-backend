f8e2ed25651b792cfa1b13a9f416f6d823704afa
Arpon207 <aarpon207@gmail.com>
2025-01-03 19:16:35 +0600

    remove socket.io

--- index.js
+++ index.js
@@ -8,31 +8,17 @@ import userRoute from "./routes/user.js";
 import orderRoute from "./routes/order.js";
 import categoryRoute from "./routes/categories.js";
 import bodyparser from "body-parser";
-import http from "http";
-import { Server } from "socket.io";
-import ordersModel from "./models/orders.model.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
 dotenv.config();
-
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: [
-      "https://admin-mehshiq.netlify.app/",
-      "https://mehshiq.netlify.app/",
-    ], // Replace with your admin panel URL
-    methods: ["GET", "POST"],
-  },
-});
 
 //connection
 const connection = async () => {
   try {
     mongoose.set("strictQuery", true);
     await mongoose.connect(process.env.MONGO_URI);
-    server.listen(port, () => {});
+    app.listen(port, () => {});
     console.log("connected to MongoDB.");
   } catch (error) {
     throw error;
@@ -52,21 +38,6 @@ app.use("/api/reviews/", reviewRoute);
 app.use("/api/users", userRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/categories", categoryRoute);
-
-// io.on("connection", (socket) => {
-//   console.log("Admin connected:", socket.id);
-
-//   socket.on("disconnect", () => {
-//     console.log("Admin disconnected:", socket.id);
-//   });
-// });
-
-ordersModel.watch().on("change", (change) => {
-  if (change.operationType === "insert") {
-    const newOrder = change.fullDocument;
-    io.emit("newOrder", newOrder);
-  }
-});
 
 app.get("/", async (req, res) => {
   res.send("MehShiq server.");
